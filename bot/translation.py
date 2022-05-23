87f735d411df17ebba96fc36d046e1513c04c221
Hackermanker <88287526+Hackermanker@users.noreply.github.com>
2022-05-23 08:32:25 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,7 +10,7 @@ class Translation(object):
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers🤌🙂©️
 
 For More Details Click Help Button Below..
-@MovieHub
+@moviehub
 </i>"""    
     
     HELP_TEXT = """
@@ -43,7 +43,7 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@MovieHub
+@moviehub
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
